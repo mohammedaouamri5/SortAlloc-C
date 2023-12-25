5ec8c4bf5cd626d8ef4cae32e13ac1eea6ae4464
B-AJ-Amar <amarbouakaz91@gmail.com>
2023-12-25 18:51:07 +0100

    replace the old str by strKit

--- lib/str.c
+++ lib/str.c
@@ -48,7 +48,90 @@ void strExtend(str* s, char* c) {
     s->data[s->length] = '\0';
 }
 
+void strInsert(str* myStr, char* InStr, int index) {
+    int len = strlen(InStr);
+    myStr->data = realloc(myStr->data, myStr->length + len + 1);
+    memmove(myStr->data + index + len, myStr->data + index, myStr->length - index + 1);
+    memcpy(myStr->data + index, InStr, len);
+    myStr->length += len;
+    myStr->data[myStr->length] = '\0';
+}
+
+
+
 void strFree(str* s) {
+    free(s->data);
+}
+
+
+// ? ====================================================================================
+// ? Array of strings
+// ? ====================================================================================
+
+
+strArray strArrayCreate(int length) {
+    strArray s;
+    s.length = length;
+    s.data = malloc(sizeof(str) * length);
+    for (int i = 0; i < length; i++) {
+        s.data[i] = strCreate(NULL);
+    }
+    return s;
+}
+
+void strArrayPrint(strArray s) {
+    printf("[");
+    for (int i = 0; i < s.length; i++) {
+        printf("'%s', ", strGet(s.data[i]));
+    }
+    printf("]\n");
+}
+void strArraySet(strArray* s, int index, const char* strIn) {
+    if (index < s->length) {
+        strSet(&s->data[index], strIn);
+    }
+}
+
+str strArrayGet(strArray s, int index) {
+    if (index < s.length) {
+        return s.data[index];
+    }
+    return strCreate(NULL);
+}
+
+int strArraySize(strArray s) {
+    return s.length;
+}
+
+void strArrayAppend(strArray* s, char* c) {
+    s->data = realloc(s->data, sizeof(str) * (s->length + 1));
+    s->data[s->length] = strCreate(c);
+    s->length++;
+}
+
+void strArrayInsert(strArray* myStr, char* InStr, int index) {
+    if (index < myStr->length) {
+        myStr->data = realloc(myStr->data, sizeof(str) * (myStr->length + 1));
+        memmove(myStr->data + index + 1, myStr->data + index, (myStr->length - index) * sizeof(str));
+        myStr->data[index] = strCreate(InStr);
+        myStr->length++;
+    }
+}
+
+void strArrayPush(strArray* s, char* c) {
+    strArrayInsert(s, c, 0);
+}
+
+str strArrayPop(strArray* s) {
+    str temp = strArrayGet(*s, 0);
+    memmove(s->data, s->data + 1, (s->length - 1) * sizeof(str));
+    s->length--;
+    return temp;
+}
+void strArrayFree(strArray* s) {
+    for (int i = 0; i < s->length; i++) {
+        strFree(&s->data[i]);
+    }
     free(s->data);
 }
 
--- lib/str.h
+++ lib/str.h
@@ -2,6 +2,10 @@
 #define str_h
 
 #include <stdlib.h>
+
+// ? ====================================================================================
+// ? String
+// ? ====================================================================================
 typedef struct str {
     int length;
     char* data;
@@ -17,8 +21,38 @@ int strSize(str s);
 
 void strExtend(str* s, char* c);
 
+void strInsert(str* myStr, char* InStr, int index);
+
 void strFree(str* s);
 
+// TODO : strReplace , strRemove , strSubstr , strFind , strSplit  , strReverse , strTrim
 
 
+// ? ====================================================================================
+// ? Array of strings
+// ? ====================================================================================
+
+typedef struct strArray {
+    int length;
+    str* data;
+} strArray;
+
+strArray strArrayCreate(int length);
+
+void strArraySet(strArray* s, int index, const char* strIn); 
+
+void strArrayPrint(strArray s);
+str strArrayGet(strArray s, int index);
+
+int strArraySize(strArray s);
+
+void strArrayAppend(strArray* s, char* c);
+
+void strArrayInsert(strArray* myStr, char* InStr, int index);
+
+void strArrayPush(strArray* s, char* c);
+
+str strArrayPop(strArray* s);
+
+void strArrayFree(strArray* s);
 #endif
